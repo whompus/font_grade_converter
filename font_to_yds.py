4a556f63e0d1f0a55dd8afcfefaa90894d03c173
Mat <mathew.b.lee@gmail.com>
2020-07-10 18:53:02 -0600

    initial commit

---
+++ font_to_yds.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Command line tool to convert font grade to YDS
+
+from argparse import Action, ArgumentParser
+
+def create_parser():
+    parser = ArgumentParser(description="Climbing grade converter - font to YDS and font to Hueco")
+    parser.add_argument('grade', help='The grade to convert, either Font or YDS')
+    parser.add_argument('type', help="The type of climb, either freeclimb or boulder")
+    return parser
+
+
+def convert_freeclimb():
+    freeclimb_grades = {}
+    with open("/Users/whompus/projects/font_converter/freeclimb_grades.txt") as f:
+        for line in f:
+            (key, val) = line.split()
+            freeclimb_grades[str(key)] = val
+    return freeclimb_grades
+
+def convert_boulder():
+    boulder_grades = {}
+    with open("/Users/whompus/projects/font_converter/boulder_grades.txt") as f:
+        for line in f:
+            (key, val) = line.split()
+            boulder_grades[str(key.lower())] = val.lower()
+    return boulder_grades
+
+def font_grade_converter():
+    args = create_parser().parse_args()
+    grade = args.grade.lower()
+    type_of_climb = args.type.lower()
+
+    freeclimb_grades = convert_freeclimb()
+    boulder_grades = convert_boulder()
+
+    if type_of_climb == 'boulder':
+        for v_scale, font in boulder_grades.items():
+            if font == grade:
+                print(v_scale)
+    elif type_of_climb == 'freeclimb':
+        for yds, font_freeclimb in freeclimb_grades.items():
+            if font_freeclimb == grade:
+                print(yds)
+    else:
+        print(f"'{type_of_climb}' not a valid type, type needs to be freeclimb or boulder")
+
+if __name__ == "__main__":
+    font_grade_converter()
+
